0eda1bfbe65994201430b45432af202c54582cd6
MoisesCDAW <moisescamposdaw@gmail.com>
2025-02-02 14:17:35 +0000

    Implementada sección para recuperar contraseña.

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -39,24 +39,6 @@ new #[Layout('layouts.guest')] class extends Component
 
 <div>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Funcionalidad no disponible por el momento :(') }}
-
-        <br><br>
-        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
-            {{ __('Volver') }}
-        </a>
-    </div>
-</div>
-
-
-{{-- 
-*
-* NOTE: This code was commented to show another view. This functionality has not been implemented yet. 
-*
---}}
-
-{{-- <div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
@@ -71,10 +53,14 @@ new #[Layout('layouts.guest')] class extends Component
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
+                {{ __('Volver') }}
+            </a>
+
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                ENVIAR ENLACE
             </x-primary-button>
         </div>
     </form>
-</div> --}}
+</div>
